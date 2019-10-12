824c6f38d2af1cfc1ce17b69db64d313c426fcb6
italosyder <55523868+italosyder@users.noreply.github.com>
2019-10-12 05:13:57 -0300

    Starting

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+import HomePage from './components/pages/homepage/homepage.component'
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+
+  render(){
+    return(
+      <HomePage />
+    )
+    
+  }
+  
+}
+
+export default App;
